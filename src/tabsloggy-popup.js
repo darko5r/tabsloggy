33aecd02e9073468e3dea993b404852502514a89
darko5r <28720396+darko5r@users.noreply.github.com>
2023-06-27 19:33:30 +0000

    Update tabsloggy-popup.js

--- src/tabsloggy-popup.js
+++ src/tabsloggy-popup.js
@@ -12,7 +12,7 @@
 // DONE: use SAP
 // DONE: export all to ZIP
 // DONE: "Save" button fix (to work with SAP if possible)
-// TODO: Correct/add comments
+// TODO: Correct/add comments and sources
 // TODO: Cleanup code
 // TODO: Remove global variables 
 // TODO: Fix CSS / add animations 
