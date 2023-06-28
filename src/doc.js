c93724a7ac107998f8d39477b82565b98501efa6
darko5r <28720396+darko5r@users.noreply.github.com>
2023-06-28 19:43:34 +0000

    Create doc.js

---
+++ src/doc.js
@@ -0,0 +1,67 @@
+/** 
+ * @file This file contains the working process of my exporting tabs extension. New todos and dones are added every day.
+ * @description TODO/DONE comments for reminder.
+ * 
+ * DONE:
+ * @done add exporting to different formats.
+ * @done add logo.
+ * @done initialize code inside the DOMContentLoaded event handler.
+ * @done save settings.
+ * @done generate xml.
+ * @done fix generate button after navigating to settings and back.
+ * @done remove innerHTML.
+ * @done use SAP.
+ * @done export all to ZIP.
+ * @done "Save" button fix (to work with SAP if possible).
+ * 
+ * TODO:
+ * @todo remove averageTabCount function.
+ * @todo add same time/number opened websites counter.
+ * @todo add locales.
+ * @todo Correct/add comments and sources.
+ * @todo Cleanup code.
+ * @todo Remove global variables.
+ * @todo Fix CSS / add animations.
+ * @todo XML validation.
+ * @todo User auth.
+ * @todo Register/login forms.
+ */
+
+
+
+
+
+
+
+
+
+
+
+/** 
+* @file This file contains working process of my exporting tabs extension. New todos and dones are added every day.
+* @description TODO/DONE comments for reminder.
+* @todo remove averageTabCount function.
+* @todo add same time/number opened websites counter.
+* @done add exporting to different formats.
+* todo: add locales
+*/
+
+
+
+
+// DONE/TODO: add logo(<-DONE)/favicon(<-TODO)
+// DONE: initialize code inside the DOMContentLoaded event handler
+// DONE: save settings
+// DONE: generate xml
+// DONE: fix generate button after navigating to settings and back
+// DONE: remove innerHTML
+// DONE: use SAP
+// DONE: export all to ZIP
+// DONE: "Save" button fix (to work with SAP if possible)
+// TODO: Correct/add comments and sources
+// TODO: Cleanup code
+// TODO: Remove global variables 
+// TODO: Fix CSS / add animations 
+// TODO: XML validation
+// TODO: User auth
+// TODO: Register/login forms
